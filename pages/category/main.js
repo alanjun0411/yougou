7b13f0b7d6dc6a690f31c55198d7a5e9e9fefa57
jack <jackjun8090@163.com>
2020-03-01 19:34:53 +0800

    分类页基本完成

--- pages/category/main.js
+++ pages/category/main.js
@@ -1,66 +1,31 @@
-// pages/category/main.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 分类数据
+    categories: [],
+    selectID: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 加载分类数据
+    wx.ajax({
+      url: "/categories"
+    }).then((res) => {
+      this.setData({
+        categories: res.data.message
+      })
+      console.log(this.data.categories)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  categoriesChange (e) {
+    this.setData({
+      selectID: e.currentTarget.dataset.index
+    })
   }
 })
