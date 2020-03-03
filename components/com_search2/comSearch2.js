4202c24a6196b1b99f208530351e059ed0adcad1
jack <jackjun8090@163.com>
2020-03-03 09:04:49 +0800

    完成搜索页的搜索框

---
+++ components/com_search2/comSearch2.js
@@ -0,0 +1,27 @@
+// components/com_search2/comSearch2.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    inputValue: ''
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    bindKeyInput: function (e) {
+      this.setData({
+        inputValue: e.detail.value
+      })
+    },
+  }
+})
