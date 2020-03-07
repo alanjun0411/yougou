d099372fcfb9034d21505c26d2910b3d3de46cd3
jack <jackjun8090@163.com>
2020-03-07 09:49:22 +0800

    完成商品详情页的轮播图

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -5,6 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 商品ID
+    goods_id: '',
+    // 商品数据
+    goods_data: {},
+    // 轮播图当前的index
+    current: 1
 
   },
 
@@ -12,6 +18,25 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    wx.showLoading({
+      title: '加载中',
+    })
+    this.setData({
+      goods_id: options.id
+    })
+    wx.ajax({
+      url: `/goods/detail?goods_id=${options.id}`
+    }).then((res) => {
+      console.log(res.data.message)
+      this.setData({
+        goods_data: res.data.message
+      })
+      wx.hideLoading()
+    })
+  },
+  currentchange: function (e) {
+    this.setData({
+      current: e.detail.current + 1
+    })
   }
 })
